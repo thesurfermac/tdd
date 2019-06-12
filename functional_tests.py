52bda1a7c82996bea16b6a6eb09603ec42cfd47c
abhinav <mac.abhinav@gmail.com>
2019-06-12 17:30:44 +0530

    refactor home page view to use a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 import unittest
 
@@ -7,6 +9,8 @@ import unittest
 # assert 'To-Do' in browser.title, "Browser title was "+ browser.title
 #
 # browser.quit()
+from selenium.webdriver.common.keys import Keys
+
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -21,6 +25,27 @@ class NewVisitorTest(unittest.TestCase):
 
         self.assertIn('To-Do', self.browser.title)
 
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        inputbox.send_keys('Buy peacock feathers')
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text =='1: Buy peacock feathers' for row in rows)
+        )
+
+        self.fail('Finish the test!')
 
 
 if __name__ == '__main__':
